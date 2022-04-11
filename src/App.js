2cb4da6caad91a532b9271e2074928d4a27ea5ff
Mufeeda <mufisweet@gmail.com>
2022-04-11 20:52:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card,Button,ListGroupItem,ListGroup} from 'react-bootstrap'
 
 function App() {
+  const [data,setdata]=useState([]);
+  const [collection,setcollection]=useState([]);
+  const men=()=>{
+    const res=collection.filter((i)=>{
+      return(i.category==="men\'s clothing")
+    })
+    setdata(res);
+  }
+  const women=()=>{
+    const res=collection.filter((i)=>{
+      return(i.category==="women\'s clothing")
+    })
+    setdata(res);
+  }
+  const electronics=()=>{
+    const res=collection.filter((i)=>{
+      return(i.category==="electronics")
+    })
+    setdata(res);
+  }
+  const jewellery=()=>{
+    const res=collection.filter((i)=>{
+      return(i.category==="jewelery")
+    })
+    setdata(res);
+  }
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products').then((result)=>{
+      console.log(result.data);
+      setdata(result.data);
+      setcollection(result.data);
+    })
+
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <>
+  <Button variant="secondary" onClick={men}>Mens Clothing</Button>{' '}
+  <Button variant="secondary" onClick={women}>Womens Clothing</Button>{' '}
+  <Button variant="secondary" onClick={jewellery}>Jewellery</Button>{' '}
+  <Button variant="secondary" onClick={electronics}>Electronics</Button>
+    </>
+    <div className='d-flex flex-row flex-wrap'>
+   { data.map((i)=>{
+      return(
+        <Card style={{ width: '18rem', margin:'10px' }}>
+  <Card.Img variant="top" src={i.image} style={{width:'150px',height:"100px"}} />
+  <Card.Body>
+    <Card.Title>{i.title}</Card.Title>
+    
+    
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Price:{i.price}</ListGroupItem>
+    <ListGroupItem>Category:{i.category}</ListGroupItem>
+    
+  </ListGroup>
+  
+  </Card.Body>
+</Card>
+
+      )
+    })}
+    
+    
+</div>
+</>
   );
 }
 
